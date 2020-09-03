a4f3de7aa3513176b0a6980c382fa160db507856
RODOLFO DANTAS QUEIROZ <rodolfo.queiroz01@fatec.sp.gov.br>
2020-09-03 07:49:34 -0300

    Finalização de funções

---
+++ Funcao/factory.js
@@ -0,0 +1,9 @@
+//factory
+function criarPessoa(){
+    return{
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa())
---
+++ Funcao/factory2.js
@@ -0,0 +1,12 @@
+function criaProduto(nome, preco){
+    return{
+        nome,
+        preco,
+        desconto: 0.1
+    }
+}
+
+console.log(criaProduto('Notebook', '2199.0'))
+console.log(criaProduto('Notebook', '2199.0'))
+console.log(criaProduto('Notebook', '2199.0'))
+ 
